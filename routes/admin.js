ee4f83d7035386310bf342af068d6dd21593c99a
zenocodes <azronebrian@gmail.com>
2022-09-29 20:00:46 +0300

    added login functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const adminCredentials = {
+  email: 'admin@hotelsys.com',
+  password: 'test@1234!'
+}
+
+/* admin dashboard */
 router.get('/', function(req, res, next) {
   res.render('admin/dashboard', {title: 'Dashboard'});
 });
 
-// users' login page
+// display admin login page
 router.get('/login', (req, res, next) => {
-  res.render('admin/login', {title: 'Login'})
+  const admin = {
+    email: '',
+    password: ''
+  }
+  res.render('admin/login', {title: 'Login', admin: admin, error: false})
 })
 
+// submit admin login page
+router.post('/login', (req, res, next) => {
+  const admin = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  if (adminCredentials.email === admin.email) {
+    if (adminCredentials.password === admin.password) {
+      req.session.user = 'admin'
+      res.redirect('/admin')
+    } else {
+      let message = 'Incorrect password.'
+      res.render('admin/login', {title: 'Login', admin: admin, error: true, message: message})
+    }
+  } else {
+    let message = 'Email does not exist.'
+    res.render('admin/login', {title: 'Login', admin: admin, error: true, message: message})
+  }
+
+})
 
 module.exports = router;
