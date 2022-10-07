873c1bc6861f691f46b5f262b3cc5ff2cd8cfdb3
zenocodes <azronebrian@gmail.com>
2022-10-07 19:14:26 +0300

    added eventListers on menu

---
+++ public/javascripts/menu.js
@@ -0,0 +1,13 @@
+let btns = document.querySelectorAll('.btn')
+let items = document.querySelectorAll('.item')
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let id = btn.textContent.toLowerCase().trim().replace(' ', '-') 
+        let item = document.querySelector(`#${id}`)
+
+        items.forEach(item => item.style.display = 'none')
+        item.style.display = 'block'
+    })
+})
