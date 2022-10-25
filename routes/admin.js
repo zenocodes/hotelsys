a82dacfb7570dec2ad3e3e5b9f8fe649d02ca47a
zenocodes <azronebrian@gmail.com>
2022-10-25 20:15:41 +0300

    added connection query

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var multer = require('multer')
 const app = require('../app');
+const connection = require('../db-config')
 var router = express.Router();
 const uploads = multer({dest: 'public/uploads/'})
 
@@ -70,8 +71,30 @@ router.get('/create', (req, res) => {
   }
 })
 
-router.post('/create', (req, res) => {
-  
+router.post('/create', uploads.single('picture'), (req, res) => {
+  const menu = {
+    category: req.body.category,
+    name: req.body.name,
+    description: req.body.description,
+    picture: req.file.filename,
+    price: req.body.price
+  }
+
+  let sql = 'INSERT INTO menu (category, name, description, picture, price) VALUES (?,?,?,?,?)'
+  connection.query(
+    sql,
+    [ 
+      menu.category,
+      menu.name,
+      menu.description,
+      menu.picture,
+      menu.price
+    ],
+    (error, results) => {
+      console.log('menu item successfully added')
+      res.redirect('/admin')
+    }
+  )
 })
 
 module.exports = router;
