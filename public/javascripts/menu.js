745b6b8fc9c6add1da0778cbde3678cc0918f7dc
zenocodes <azronebrian@gmail.com>
2022-10-28 19:53:17 +0300

    added eventListener on all menu items

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,5 +1,7 @@
 let btns = document.querySelectorAll('.btn')
 let items = document.querySelectorAll('.item')
+let orderList = document.querySelector('#order-list')
+let menus = document.querySelectorAll('.menu')
 
 
 btns.forEach(btn => {
@@ -9,5 +11,15 @@ btns.forEach(btn => {
 
         items.forEach(item => item.style.display = 'none')
         item.style.display = 'flex'
+    })
+})
+
+menus.forEach(menu => {
+    menu.addEventListener('click', () => {
+        let li = document.createElement('li')
+        let itemName = menu.children[1].textContent
+        let itemPrice = menu.lastElementChild.textContent.split('KES ')[1]
+        li.innerHTML = `${itemName} - <span>${itemPrice}</span>`
+        orderList.appendChild(li)
     })
 })
